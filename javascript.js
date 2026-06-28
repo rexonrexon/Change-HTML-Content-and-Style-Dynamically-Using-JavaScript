09a7c9e2144edf7503f10ce173e4ba59380f80ed
rexonrexon <rexonrexon2924@gmail.com>
2026-06-28 14:42:16 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function changeContent(){
+
+    document.getElementById("heading").innerHTML =
+    "DOM Manipulation Successful";
+
+    document.getElementById("message").innerHTML =
+    "The content and style have been changed dynamically.";
+
+    document.getElementById("heading").style.color = "blue";
+
+    document.getElementById("message").style.color = "green";
+
+    document.body.style.backgroundColor = "lightyellow";
+}
+
+
+
+
+
+
+
+
+
+
+
+
